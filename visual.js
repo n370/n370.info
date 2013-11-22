5026caeb7d97c30750df047eead2f1f2f7061ceb
Dylson Valente Neto <m@n370.info>
2013-11-22 02:10:39 -0300

    Pequenas mudanças

--- visual.js
+++ visual.js
@@ -2,6 +2,6 @@
 var paper = Raphael(0, 0, 320, 200);
 
 // Creates circle at x = 50, y = 40, with radius 10
-var circle = paper.circle(0, 0, 100);
+var circle = paper.circle(200, 100, 10);
 // Sets the fill attribute of the circle to red (#f00)
 circle.attr("fill", "black");
