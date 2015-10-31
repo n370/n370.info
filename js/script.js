c464ba24175e963c140da3ece21d49ae45504384
Dylson Valente Neto <dylsonvalenteneto@gmail.com>
2015-10-30 22:00:54 -0300

    Hack a bit more and add some original content

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 window.onresize = window.onload = function() {
   var main = document.getElementsByTagName('main')[0];
   var header = document.getElementsByTagName('header')[0];
+  var footer = document.getElementsByTagName('footer')[0];
+
   main.style.marginTop = window.getComputedStyle(header).height
+  main.style.marginBottom = window.getComputedStyle(footer).height
 }
