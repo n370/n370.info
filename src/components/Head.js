1ea3fe331b631e4d9e1b040bcd60f4927502638c
Dylson Valente Neto <ama@n370.info>
2019-11-20 21:38:20 -0300

    Add pinterest meta tag

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 
 export default ({ title }) => (
   <Helmet>
+    <meta name="p:domain_verify" content="c35f4a3ff9f84cc23df833202f6a8cc5"/>
     <link href="https://fonts.googleapis.com/css?family=Viga" rel="stylesheet" />
     <link rel="shortcut icon" href="images/favicons/favicon.ico"/>
     <title>{title}</title>
