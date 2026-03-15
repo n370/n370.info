72f6edd30cd649f9c45e8b3fe450402e67cfd865
Dylson Valente Neto <ama@n370.info>
2026-03-14 23:19:05 -0300

    config static export

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  output: 'export',
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
